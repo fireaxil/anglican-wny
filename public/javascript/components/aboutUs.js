d6a9da3f122bc9635b4d0cfb136a87663beb74d0
Christian Koehler <sandvice@gmail.com>
2017-12-29 20:00:51 -0500

    updated text

--- public/javascript/components/aboutUs.js
+++ public/javascript/components/aboutUs.js
@@ -13,16 +13,20 @@ const styles = theme => ({
     marginLeft: 'auto',
     marginRight: 'auto'
   },
+  paragraphs: {
+    marginBottom: 20
+  },
   text: {
     fontWeight: 300,
     lineHeight: '1.6em',
     paddingLeft: 24,
-    paddingRight: 24
+    paddingRight: 24,
+    textAlign: 'center'
   },
   '@media (min-width: 948px)': {
     text: {
-      paddingLeft: '20%',
-      paddingRight: '20%'
+      paddingLeft: '19%',
+      paddingRight: '19%'
     }
   },
   divider: {
@@ -50,11 +54,68 @@ class AboutUs extends React.Component {
         </Typography>
         <br />
         <Typography className={classes.text} type='body1'>
-          We walk in the way of Jesus because we need something new. Our culture, our media, our internet all feed us many new things, but none renews our minds like Jesus does.
-          When a seed falls into the ground, it dies in a sense, but then it sprouts to a totally new life. When Jesus renews our minds, he begins a new (and sometimes hard) process of planting a whole new garden in the world. We are here to support each other as God works in us, and to taste the good fruit of the kingdom of God as he grows it in our lives, neighborhoods, and families.
-          The particular way we do this comes from a pattern called the Anglican tradition.
+          <div className={classes.paragraphs}>
+            <i>"Be transformed by the renewing of your mind.”</i> <br />
+            — St. Paul, Letter to the Romans <br />
+          </div>
+          <div className={classes.paragraphs}>
+            We walk in the way of Jesus because we need something new.
+            Our culture, our media, our internet all feed us many new things, but none renews our
+            minds like Jesus does. <br />
+          </div>
+          <div className={classes.paragraphs}>
+            When a seed falls into the ground, it dies in a sense,
+            but then it sprouts to a totally new life.
+            When Jesus renews our minds, he begins a new (and sometimes hard) process of planting a
+            whole new garden in the world. We are here to support each other as God works in us, and
+            to taste the good fruit of the kingdom of God as he grows it in our lives, neighborhoods
+            and families. <br />
+          </div>
+          <div className={classes.paragraphs}>
+            The particular way we do this comes from a pattern called the Anglican tradition
+          </div>
+          <Typography className={classes.centerText} type='headline'>
+            What is the Anglican tradition of following Jesus?
+          </Typography>
+          <div className={classes.paragraphs}>
+            The Anglican tradition draws from the deepest wells of Christian spirituality and community.
+            If you are familiar with Roman Catholic worship, you might recognize our liturgical
+            responses ("The Lord be with you"). But if you are familiar with Protestant or evangelical
+            worship you would appreciate the personal nature. If you have a background in other faiths
+            or none at all, you might see that we advocate strongly for the poor and excluded, but
+            we also never stop talking about being transformed in our own lives. <br />
+          </div>
+          <div className={classes.paragraphs}>
+            All of this is simply our way of talking about the explosive,
+            revolutionary power of the life, death, and resurrection of Jesus Christ. <br />
+          </div>
+          <div className={classes.paragraphs}>
+            <i>“All Christian Ways are like cups. There are many kinds of cups in the world,
+              small and large, made of glass, pottery, etc. ..." </i> <br />
+          </div>
+          <div className={classes.paragraphs}>
+            <i>
+              Think of pouring wine into a cup. The wine itself is independent of the cup, but experts will tell you
+              that the kind of cup you pour wine into will affect the taste. ...
+            </i>
+            <br />
+          </div>
+          <div className={classes.paragraphs}>
+            <i>
+              "It’s best to put wine into the right kind of cup,
+              but the cup isn’t the most important thing. The wine is what matters.
+              The Anglican Way is a cup. The Gospel of Jesus Christ is the wine.
+              The Anglican Way is a good cup into which to pour this most magnificent of wines,
+              but it isn’t nearly as important as the wine itself. It isn’t the only kind of glass
+              you could use, but it is a good one, and one that does a nice job of allowing the
+              wine to be experienced as it was meant to be.” <br />
+            </i>
+            — Thomas McKenzie, The Anglican Way <br />
+          </div>
+          Church of the Atonement is part of the Anglican Church in North America,
+          through which we are connected to the worldwide Anglican Communion.
         </Typography>
-        <div style={{width: '100%', display: 'flex', justifyContent:'center'}}>
+        <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
           <Divider className={classes.divider} />
         </div>
         <Typography className={classes.centerText} type='display2'>
